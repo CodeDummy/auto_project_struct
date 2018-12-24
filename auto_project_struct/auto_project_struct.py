8d268fad6493cff2eb28904af8aabf947367b3f5
CodeDummy <kl17a9161@gmail.com>
2018-12-24 21:30:07 +0100

    added code to create setup.py

--- auto_project_struct/auto_project_struct.py
+++ auto_project_struct/auto_project_struct.py
@@ -6,34 +6,36 @@ import os
 
 # TODO get the folder path from the user
 
-PATH = "C://code/testfolder/"
+PATH = "C://code/newtest/"
 stringlist = PATH.split("/")
 SCRIPT_NAME = stringlist[-2]
 # print(stringlist[-2])
 
-#Create path for the main script
+# Create path for the main script
 SRC_PATH = PATH + SCRIPT_NAME + "/"
 os.makedirs(SRC_PATH)
 src = open(SRC_PATH + "testfolder.py", "w")
 src.write("#Write the source code in file.This is the script that you’re distributing")
-src.write("#As far as naming the main script file goes,I recommend that you go with the name of your project") 
+src.write(
+    "#As far as naming the main script file goes,I recommend that you go with the name of your project"
+)
 src.write("which is the same as the name of the top-level directory")
 src.close()
 
 
-#create helper.py
-helper = open(SRC_PATH+"helpers.py", "w")
+# create helper.py
+helper = open(SRC_PATH + "helpers.py", "w")
 helper.write("#this file is for all the helper methods")
 helper.close()
 
- #create _init_.py
+# create _init_.py
 init = open(SRC_PATH + "_init_.py", "w")
 
-#Create path for the docs
+# Create path for the docs
 DOC_PATH = PATH + "docs"
 os.mkdir(DOC_PATH)
 
-#Create path for tests
+# Create path for tests
 TEST_PATH = PATH + "tests/"
 os.mkdir(TEST_PATH)
 tests = open(TEST_PATH + "/tests.py", "w")
@@ -45,4 +47,10 @@ req.write(
     "# This file defines outside Python dependencies and their versions for your application."
 )
 req.close()
+
+#create setup.py
+setp = open(PATH + "setup.py", "w")
+
+#create .gitignore 
+#setp = open(PATH + ".gitignore", "w")
 
