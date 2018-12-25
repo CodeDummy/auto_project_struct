74aacd0d8b637c7307c7e3ee59d7694f243522b7
CodeDummy <kl17a9161@gmail.com>
2018-12-25 16:25:12 +0100

    implemented CLI with proJ_name as argument.

--- auto_project_struct/auto_project_struct.py
+++ auto_project_struct/auto_project_struct.py
@@ -3,54 +3,65 @@
 
 import pathlib
 import os
-
-# TODO get the folder path from the user
-
-PATH = "C://code/newtest/"
-stringlist = PATH.split("/")
-SCRIPT_NAME = stringlist[-2]
-# print(stringlist[-2])
-
-# Create path for the main script
-SRC_PATH = PATH + SCRIPT_NAME + "/"
-os.makedirs(SRC_PATH)
-src = open(SRC_PATH + "testfolder.py", "w")
-src.write("#Write the source code in file.This is the script that you’re distributing")
-src.write(
-    "#As far as naming the main script file goes,I recommend that you go with the name of your project"
-)
-src.write("which is the same as the name of the top-level directory")
-src.close()
+import click
 
 
-# create helper.py
-helper = open(SRC_PATH + "helpers.py", "w")
-helper.write("#this file is for all the helper methods")
-helper.close()
+@click.command()
+@click.argument('proj_name')
 
-# create _init_.py
-init = open(SRC_PATH + "_init_.py", "w")
+def main(proj_name) : #(proj_type) :
 
-# Create path for the docs
-DOC_PATH = PATH + "docs"
-os.mkdir(DOC_PATH)
+    # TODO get the folder path from the user
 
-# Create path for tests
-TEST_PATH = PATH + "tests/"
-os.mkdir(TEST_PATH)
-tests = open(TEST_PATH + "/tests.py", "w")
+    PATH = "C://code/"+proj_name+"/"
+    stringlist = PATH.split("/")
+    SCRIPT_NAME = stringlist[-2]
+    # print(stringlist[-2])
+    
+    # Create path for the main script
+    SRC_PATH = PATH + SCRIPT_NAME + "/"
+    os.makedirs(SRC_PATH)
+    src = open(SRC_PATH + proj_name+".py", "w")
+    src.write("#Write the source code in file.This is the script that you’re distributing")
+    src.write(
+        "#As far as naming the main script file goes,I recommend that you go with the name of your project"
+    )
+    src.write("which is the same as the name of the top-level directory")
+    src.close()
+    
+    
+    # create helper.py
+    helper = open(SRC_PATH + "helpers.py", "w")
+    helper.write("#this file is for all the helper methods")
+    helper.close()
+    
+    # create _init_.py
+    init = open(SRC_PATH + "_init_.py", "w")
+    
+    # Create path for the docs
+    DOC_PATH = PATH + "docs"
+    os.mkdir(DOC_PATH)
+    
+    # Create path for tests
+    TEST_PATH = PATH + "tests/"
+    os.mkdir(TEST_PATH)
+    tests = open(TEST_PATH + "/tests.py", "w")
+    
+    
+    # Create requirements.txt and write a comment
+    req = open(PATH + "requirements.txt", "w")
+    req.write(
+        "# This file defines outside Python dependencies and their versions for your application."
+    )
+    req.close()
 
+    # create setup.py
+    setp = open(PATH + "setup.py", "w")
+    
+    
+    # TODO create .gitignore
+    # create .gitignore
+    # setp = open(PATH + ".gitignore", "w")
 
-# Create requirements.txt and write a comment
-req = open(PATH + "requirements.txt", "w")
-req.write(
-    "# This file defines outside Python dependencies and their versions for your application."
-)
-req.close()
-
-#create setup.py
-setp = open(PATH + "setup.py", "w")
-
-#create .gitignore 
-#setp = open(PATH + ".gitignore", "w")
-
+if __name__ == "__main__":
+    main()
