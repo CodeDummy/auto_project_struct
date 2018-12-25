450e82acd5d5e9c2943604288003d4f032737604
CodeDummy <kl17a9161@gmail.com>
2018-12-25 16:52:01 +0100

    Added basic exception handling.Throws out a message if an old script with the same name exists in the same path.

--- auto_project_struct/auto_project_struct.py
+++ auto_project_struct/auto_project_struct.py
@@ -7,61 +7,67 @@ import click
 
 
 @click.command()
-@click.argument('proj_name')
-
-def main(proj_name) : #(proj_type) :
+@click.argument("script_name")
+def main(script_name):  # (proj_type) :
 
     # TODO get the folder path from the user
+    # TODO add functionality to delete the structure with CLI optional parameter
 
-    PATH = "C://code/"+proj_name+"/"
-    stringlist = PATH.split("/")
-    SCRIPT_NAME = stringlist[-2]
+    PATH = "C://code/" + script_name + "/"
+    # stringlist = PATH.split("/")
+    SCRIPT_NAME = script_name
     # print(stringlist[-2])
-    
+
     # Create path for the main script
     SRC_PATH = PATH + SCRIPT_NAME + "/"
-    os.makedirs(SRC_PATH)
-    src = open(SRC_PATH + proj_name+".py", "w")
-    src.write("#Write the source code in file.This is the script that you’re distributing")
-    src.write(
-        "#As far as naming the main script file goes,I recommend that you go with the name of your project"
-    )
-    src.write("which is the same as the name of the top-level directory")
-    src.close()
+    try :
+        os.makedirs(SRC_PATH)
+        src = open(SRC_PATH + script_name + ".py", "w")
+        src.write(
+            "#Write the source code in file.This is the script that you’re distributing"
+        )
+        src.write(
+            "#As far as naming the main script file goes,I recommend that you go with the name of your project"
+        )
+        src.write("which is the same as the name of the top-level directory")
+        src.close()
     
+        # create helper.py
+        helper = open(SRC_PATH + "helpers.py", "w")
+        helper.write("#this file is for all the helper methods")
+        helper.close()
     
-    # create helper.py
-    helper = open(SRC_PATH + "helpers.py", "w")
-    helper.write("#this file is for all the helper methods")
-    helper.close()
+        # create _init_.py
+        init = open(SRC_PATH + "_init_.py", "w")
     
-    # create _init_.py
-    init = open(SRC_PATH + "_init_.py", "w")
+        # Create path for the docs
+        DOC_PATH = PATH + "docs"
+        os.mkdir(DOC_PATH)
     
-    # Create path for the docs
-    DOC_PATH = PATH + "docs"
-    os.mkdir(DOC_PATH)
+        # Create path for tests
+        TEST_PATH = PATH + "tests/"
+        os.mkdir(TEST_PATH)
+        tests = open(TEST_PATH + "/tests.py", "w")
+        tests.write("#write all your tests for this simple script here")
+        tests.close()
     
-    # Create path for tests
-    TEST_PATH = PATH + "tests/"
-    os.mkdir(TEST_PATH)
-    tests = open(TEST_PATH + "/tests.py", "w")
+        # Create requirements.txt and write a comment
+        req = open(PATH + "requirements.txt", "w")
+        req.write(
+            "# This file defines outside Python dependencies and their versions for your application."
+        )
+        req.close()
     
+        # create setup.py
+        setp = open(PATH + "setup.py", "w")
     
-    # Create requirements.txt and write a comment
-    req = open(PATH + "requirements.txt", "w")
-    req.write(
-        "# This file defines outside Python dependencies and their versions for your application."
-    )
-    req.close()
+        # TODO create .gitignore
+        # create .gitignore
+        # setp = open(PATH + ".gitignore", "w")
+    except:
+        print("A script with the same name exists, C'mon be creative!")    
+    
 
-    # create setup.py
-    setp = open(PATH + "setup.py", "w")
-    
-    
-    # TODO create .gitignore
-    # create .gitignore
-    # setp = open(PATH + ".gitignore", "w")
 
 if __name__ == "__main__":
     main()
